9906a4718fa3408b1704b4ad7ff5ab7a657abc05
Ngô Trung Chiến <ngotrungchien232@gmail.com>
2024-11-20 22:11:49 +0700

    C_Session03_Ex01

---
+++ Session02_Ex01.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+// thu vien chuan cua c
+
+int main(){
+	char address[100];
+	printf("Moi ban nhap ho va ten: ");
+    gets(address);
+	printf("Xin chao %s", address);
+	return 0;
+}
